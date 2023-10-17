d761e467683643d15fb5bae3349839c39976f5c4
KeineWieDu <578586993@qq.com>
2023-10-17 21:12:55 +0800

    feat: 实现部分的错误处理

--- Result/Error/AnalysisErrorType.java
+++ Result/Error/AnalysisErrorType.java
@@ -4,7 +4,13 @@ package Result.Error;
  * 错误种类的枚举
  */
 public enum AnalysisErrorType {
+    UNEXPECTED_ERROR, // 预料之外的其他错误
     ILLEGAL_SYMBOL, // 非法符号
     NAME_REPETITION, // 名字重定义
     ASSIGN_TYPE_NOT_MATCH, // 赋值类型不匹配
+    IDENTIFIER_NOT_DEFINE, // 标识符未定义
+    CHANGE_CONST_VALUE, // 改变常量的值
+    ARRAY_INDEX_OUT_OF_BOUND, // 数组下标越界
+    VALUE_NOT_INITIALIZE, // 值未初始化
+    ARRAY_DIMENSION_NOT_MATCH, // 数组维度不匹配，比如说试图访问一维数组第二维的值或者相反的
 }
--- Result/Error/HandleError.java
+++ Result/Error/HandleError.java
@@ -1,8 +1,15 @@
 package Result.Error;
 
+import Lexical.LexicalAnalysis;
+import Output.OutputIntoFile;
+
 public class HandleError {
 
     public static void lexicalError() {
         System.out.println("error");
+    }
+
+    public static void handleError(AnalysisErrorType errorType) {
+        OutputIntoFile.appendToFile(LexicalAnalysis.getInstance().getCurrentLine() + " " + errorType.ordinal(), "output.txt");
     }
 }
